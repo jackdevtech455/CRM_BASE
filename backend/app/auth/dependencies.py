fdfd799bb9feaa58c55e65ee2b62a62bdf0388ad
Jack <jackdevtech455@gmail.com>
2026-07-16 00:37:51 +0100

    fix circular dependency between ddependency and auth.users

---
+++ backend/app/auth/dependencies.py
@@ -0,0 +1,12 @@
+from typing import Annotated
+
+from fastapi import Depends
+from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+
+AccessTokenDependency = Annotated[str, Depends(oauth2_scheme)]
+OAuth2PasswordRequestFormDependency = Annotated[
+    OAuth2PasswordRequestForm,
+    Depends(OAuth2PasswordRequestForm),
+]
