426ead780dea8996cd6e9963df3944ef3a529221
Jack <jackdevtech455@gmail.com>
2026-07-16 00:00:02 +0100

    fixed env file name in dockerfile

--- backend/app/api/dependencies.py
+++ backend/app/api/dependencies.py
@@ -4,11 +4,11 @@ Shared dependencies for use in routes.
 
 from typing import Annotated
 
-from backend.app.db.session import get_db
 from fastapi import Depends
 from sqlalchemy.orm import Session
 
 from app.auth.users import get_current_user
+from app.db.session import get_db
 from app.models.user import User
 
 DatabaseDependency = Annotated[Session, Depends(get_db)]
