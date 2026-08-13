38aa791b076f36038c7c50434042e3b7eeae3e41
Jack <jackdevtech455@gmail.com>
2026-08-13 23:01:13 +0100

    added /api to oauth2 scheme

--- backend/app/auth/dependencies.py
+++ backend/app/auth/dependencies.py
@@ -7,7 +7,7 @@ from typing import Annotated
 from fastapi import Depends
 from fastapi.security import OAuth2PasswordBearer, OAuth2PasswordRequestForm
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/api/auth/login")
 
 AccessTokenDependency = Annotated[str, Depends(oauth2_scheme)]
 OAuth2PasswordRequestFormDependency = Annotated[
