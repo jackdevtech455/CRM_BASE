71c60f154b2244c74e94fa12f506b57d61a465ca
Jack <jackdevtech455@gmail.com>
2026-08-18 22:27:32 +0100

    added tickets to dasboard

--- frontend/src/types/dashboard.ts
+++ frontend/src/types/dashboard.ts
@@ -1,9 +1,9 @@
 import type { Client } from "./client";
-// import type { Ticket } from "./ticket";
+import type { Ticket } from "./ticket";
 
 export type Dashboard = {
   client_count: number;
   ticket_count: number;
   recent_clients: Client[];
-  // recent_tickets: Ticket[];
+  recent_tickets: Ticket[];
 };
